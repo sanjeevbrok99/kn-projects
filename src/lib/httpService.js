bc69a2c1d28942cdc4faa014b16fe253c3c5743f
snipextt <snipextt@gmail.com>
2021-12-12 19:22:01 +0530

    Api for employee and departments

--- src/lib/httpService.js
+++ src/lib/httpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpService = axios.create({
-  baseURL: 'https://application-knmulti.herokuapp.com/api/v1/',
+  baseURL: 'https://application-knmulti.herokuapp.com/api/v1',
 });
 
 export default httpService;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import employeeReducer from '../features/employee/employeeSlice';
+import departmentReducer from '../features/department/departmentSlice';
 
 export default configureStore({
   reducer: {
     employee: employeeReducer,
+    department: departmentReducer,
   },
 });
